e3aa5880a0429fafcb4a5f2503812d149d4bc288
hazartilirot <hazartilirot@gmail.com>
2022-01-02 00:55:45 +0200

    A varaible scope is somewhat similar to JS. Letter case matters for exporting valus and func, struct looks like an interface in TS

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ func name(<&argument)
 func name (<parameter *type>)
 ```
 
+a short variable declaration
+```
+:= 
+```
+
+A variable scope seems to be somewhat similar to JS. At least what I'm shown 
+in **types-structs** lesson
+
+A letter case matters for exporting values and function. If a name begins 
+with a capital letter it might be available outside the current package.
+
+**struct** looks like an interface in Typescript
+
---
+++ types-structs/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+var s = "seven"
+
+func saySomething(s string) (string, string) {
+	log.Print("s from the saySomething func is ", s)
+	return s, "World"
+}
+
+type User struct {
+	FirstName string
+	LastName string
+	PhoneMumber string
+	Age int
+	BirthDate time.Time
+}
+
+func main() {
+	var s2 = "six"
+	log.Print("s is ", s)
+	log.Print("s2 is ", s2)
+	
+	saySomething("xxx")
+	
+	user := User {
+		FirstName: "Trevor",
+		LastName: "Sawler",
+	}
+	
+	log.Println(user.FirstName, user.LastName)
+}
